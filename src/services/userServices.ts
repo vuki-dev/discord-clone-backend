0ce593b22afdcd908dce47efb5d6b23090b11543
vukasin <vukasin.pavlovic345@gmail.com>
2024-06-18 17:56:51 +0200

    small bugfix

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -1,10 +1,12 @@
+import { RowDataPacket } from "mysql2";
 import db, { executeQuery } from "../db/dbConfig";
+import { UserType } from "../utils/types";
 
 export const getUserById = async (userId: string | unknown) => {
-  const user = await executeQuery(
+  const user = (await executeQuery(
     "SELECT id, name, email, created_at FROM users WHERE id = ?",
     [userId]
-  );
+  ) as RowDataPacket[])[0];
 
   return user;
 };
